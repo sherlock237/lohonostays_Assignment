50dc806c0e10699106790d7e21c92d4c17bfb1cd
achal-bhadada <achalbhadada55@gmail.com>
2023-10-08 01:44:04 +0530

    added the routes for both the APIs

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+
+  Rails.application.routes.draw do
+    resources :villas, only: [:index, :show]
+  end
 end

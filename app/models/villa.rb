704c76ee7add495dfe8c8c80857a58836a0f0bfa
achal-bhadada <achalbhadada55@gmail.com>
2023-10-08 11:32:34 +0530

    updated price/availabilty funcs in models

--- app/models/villa.rb
+++ app/models/villa.rb
@@ -1,3 +1,17 @@
 class Villa < ApplicationRecord
-  has_many :calendar_entries   
+  has_many :calendar_entries
+
+  def is_available?(start_date, end_date)
+    (start_date...end_date).all? do |date|
+      calendars.where(date: date, availability: true).exists?
+    end
+  end
+
+  def average_price(start_date, end_date)
+    calendars.where(date: start_date...end_date).average(:price)
+  end
+
+  def total_price(start_date, end_date)
+    calendars.where(date: start_date...end_date).sum(:price)
+  end
 end

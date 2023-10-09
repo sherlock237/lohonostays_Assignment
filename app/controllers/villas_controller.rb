04dec55059e5b62c0a54f06734b55d6502069395
achal-bhadada <achalbhadada55@gmail.com>
2023-10-09 12:24:14 +0530

    basic error handling

--- app/controllers/villas_controller.rb
+++ app/controllers/villas_controller.rb
@@ -4,25 +4,39 @@ class VillasController < ApplicationController
 
   def index
     sort_by = params[:sort_by]
-    villas_details = VillaService.get_villas(@start_date, @end_date, sort_by)
-
-    render json: villas_details
+    begin
+      villas_details = VillaService.get_villas(@start_date, @end_date, sort_by)
+      render json: villas_details
+    rescue => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
   end
 
   def show
-    villa_info = VillaService.get_villa_info(@villa, @start_date, @end_date)
-
-    render json: villa_info
+    begin
+      villa_info = VillaService.get_villa_info(@villa, @start_date, @end_date)
+      render json: villa_info
+    rescue => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_villa
-    @villa = Villa.find(params[:id])
+    begin
+      @villa = Villa.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: 'Villa not found' }, status: :not_found
+    end
   end
 
   def parse_dates
-    @start_date = Date.parse(params[:start_date])
-    @end_date = Date.parse(params[:end_date])
+    begin
+      @start_date = Date.parse(params[:start_date])
+      @end_date = Date.parse(params[:end_date])
+    rescue ArgumentError => e
+      render json: { error: 'Invalid date format' }, status: :bad_request
+    end
   end
 end
